7c00108e1c77024897f145fb97500f10ce8b2607
Antonio Ricardo <antonio.eschola@gmail.com>
2015-09-16 11:07:22 -0400

    Add users

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -3,7 +3,7 @@ class ManageController < ApplicationController
   before_action :authenticate_user!
    	
   def admin
-    if current_user.login == 'barellywgp' || current_user.login == 'bazzananas' || current_user.login == 'kaiquejcs' || current_user.login == 'mirallesfvo' || current_user.login == 'cordobalm'
+    if current_user.login == 'barellywgp' || current_user.login == 'bazzananas' || current_user.login == 'kaiquejcs' || current_user.login == 'mirallesvfo' || current_user.login == 'cordobalm'
       redirect_to :controller => 'manage', :action => 'stay'
     elsif current_user.login == 'ricardosouzaf' || current_user.login == 'vanonidv' || current_user.login == 'zagor' || current_user.login == 'martinsjb' || current_user.login == 'massariolmc' || current_user.login == 'patrickls' || current_user.login == 'anacarolinams'   	
     #permanencia
